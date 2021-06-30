3d723ecb076d39a209edf37d38eab9cc867e4342
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-29 22:03:52 -0300

    tirando bugs

--- registro_hora_extra/models.py
+++ registro_hora_extra/models.py
@@ -5,12 +5,11 @@ from django.urls import reverse
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
-    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE)
     hora = models.DecimalField(max_digits=5, decimal_places=2)
 
     def get_absolute_url(self):
         return reverse('list_hora_extra')
-
 
     def __str__(self):
         return self.motivo
